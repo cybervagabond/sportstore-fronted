53cc4c68b5918248729899b0d37eac74a0f1b20b
cybervgb <a.shaidullin@rdl-telecom.com>
2019-05-23 15:30:52 +0200

    safe place

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-    <store/>
+    <store />
 </template>
 
 <script>
-    import Store from './components/Store.vue'
-
+    import Store from "./components/Store";
+    import { mapActions } from "vuex";
     export default {
         name: 'app',
-        components: {
-            Store
+        components: { Store },
+        methods: {
+            ...mapActions(["getData"])
+        },
+        created() {
+            this.getData();
         }
     }
 </script>
-
-<!--<style>-->
-<!--    #app {-->
-<!--        font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
-<!--        -webkit-font-smoothing: antialiased;-->
-<!--        -moz-osx-font-smoothing: grayscale;-->
-<!--        text-align: center;-->
-<!--        color: #2c3e50;-->
-<!--        margin-top: 60px;-->
-<!--    }-->
-<!--</style>-->
