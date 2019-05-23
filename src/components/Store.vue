743b6a6cb9f5aa7561d0a16edc40af05fe829b09
cybervgb <a.shaidullin@rdl-telecom.com>
2019-05-23 07:07:20 +0200

    Register ProductList in Store component

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -9,6 +9,9 @@
             <div class="col-3 bg-info p-2">
                 <h4 class="text-white m-2">Categories</h4>
             </div>
+            <div class="col-9 p-2">
+                <product-list/>
+            </div>
             <div class="col-9 bg-success p-2">
                 <h4 class="text-white m-2">Products</h4>
             </div>
@@ -16,3 +19,10 @@
     </div>
 </template>
 
+<script>
+    import ProductList from "./ProductList";
+
+    export default {
+        components: {ProductList}
+    }
+</script>
