c553d552e6186604a685e7eb39a9f28672cd48e6
cybervgb <a.shaidullin@rdl-telecom.com>
2019-05-24 17:43:44 +0200

    Checkout & Order done

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import VueRouter  from "vue-router";
 
 import Store from "../components/Store";
 import ShoppingCart from "../components/ShoppingCart";
+import Checkout from "../components/Checkout";
+import OrderThanks from "../components/OrderThanks";
 
 Vue.use(VueRouter);
 
@@ -10,7 +12,9 @@ export default new VueRouter({
     mode: "history",
     routes: [
         { path: "/", component: Store },
-        { path: "/cart", component: ShoppingCart},
-        { path: "*", redirect: "/"},
+        { path: "/cart", component: ShoppingCart },
+        { path: "/checkout", component: Checkout},
+        { path: "/thanks/:id", component: OrderThanks},
+        { path: "*", redirect: "/"}
     ]
 })
