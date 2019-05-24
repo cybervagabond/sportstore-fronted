af0ac4c500605d63d0617dd38ea16781deb189e7
cybervgb <a.shaidullin@rdl-telecom.com>
2019-05-24 15:17:09 +0200

    Save orders after reload page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-    <router-view/>
+    <router-view />
 </template>
 
 <script>
-    // import Store from "./components/Store";
     import { mapActions } from "vuex";
     export default {
         name: 'app',
-        // components: { Store },
         methods: {
-            ...mapActions(["getData"])
+            ...mapActions({
+                getData: "getData",
+                initializeCart: "cart/initializeCart"
+            })
         },
         created() {
             this.getData();
+            this.initializeCart(this.$store);
         }
     }
 </script>
