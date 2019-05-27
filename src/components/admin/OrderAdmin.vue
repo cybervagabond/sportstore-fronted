a2a4889718e14a66d66ac1901db6b6360028e8e4
cybervgb <a.shaidullin@rdl-telecom.com>
2019-05-27 11:34:52 +0200

    Start implement admin page

---
+++ src/components/admin/OrderAdmin.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "OrderAdmin"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/admin/ProductAdmin.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ProductAdmin"
+    }
+</script>
+
+<style scoped>
+
+</style>
